e079500387f407884a41e34911a2e36de765fb3a
wei <wei02919@gmail.com>
2023-10-11 15:38:14 +0800

    change color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ export default function Navbar(){
   console.log(links)
   // {links.map(link=> )}
   return(
-    <nav className="bg-orange-100">
+    <nav className="bg-cyan-100">
       <div className='mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
         <h2 className='text-3xl font-bold'>
-          Web<span className='text-orange-600'>Dev</span>
+          Web<span className='text-cyan-600'>Dev</span>
         </h2>
         <div className='flex gap-x-3'>
           {links.map(link=>{
@@ -15,7 +15,7 @@ export default function Navbar(){
               <a
                 key={id}
                 href={href}
-                className='capitalize text-lg tracking-wide hover:text-orange-600 duration-300'
+                className='capitalize text-lg tracking-wide hover:text-cyan-600 duration-300'
                 >
                 {text}
                 </a>
